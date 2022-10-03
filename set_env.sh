ec1714681e7fad10f5478d62b65db7e11bdb1928
Yooxos <rihane.youssef@gmail.com>
2022-10-03 16:49:25 +0100

    Second commit

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=projet3
-export POSTGRES_PASSWORD=1593578624
+export POSTGRES_USERNAME=udagramprojet3
+export POSTGRES_PASSWORD=udagramprojet3
 export POSTGRES_HOST=projet3.cx6u5tndykfk.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=projet3
-export AWS_BUCKET=test-352878037270-dev
-export AWS_REGION=us-east-1
+export POSTGRES_DB=udagramprojet3
+export AWS_BUCKET=projet3-352878037270
+export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
